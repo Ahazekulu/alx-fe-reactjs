b8ff780d1c3f3a817367e26a54379b41bd45c56b
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-02-15 09:28:20 +0300

    Create app.jsx

---
+++ alx-react-app/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage'; // Import the WelcomeMessage component
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage /> {/* Render the WelcomeMessage component */}
+    </div>
+  );
+}
+
+export default App;
