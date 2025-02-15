62d0c543fa7db4a0cc236bde844c7e90f96a54e2
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-02-15 09:26:17 +0300

    Update app.js

--- alx-react-app/app.js
+++ alx-react-app/app.js
@@ -1,1 +1,12 @@
-React js
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage'; // Import the WelcomeMessage component
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage /> {/* Use the WelcomeMessage component */}
+    </div>
+  );
+}
+
+export default App;
