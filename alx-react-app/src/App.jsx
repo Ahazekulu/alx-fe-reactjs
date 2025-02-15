57a7189e79b5fa78e0bdf4aad7c23e18a2ab057b
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-02-15 09:33:13 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -10,3 +10,22 @@ function App() {
 }
 
 export default App;
+
+
+
+import React from 'react';
+import Header from './Header'; // Import the Header component
+import MainContent from './MainContent'; // Import the MainContent component
+import Footer from './Footer'; // Import the Footer component
+
+function App() {
+  return (
+    <div>
+      <Header /> {/* Include the Header component */}
+      <MainContent /> {/* Include the MainContent component */}
+      <Footer /> {/* Include the Footer component */}
+    </div>
+  );
+}
+
+export default App;
