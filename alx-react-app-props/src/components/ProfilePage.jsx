02e8300f0eb34c55c270a211959f1ac849f231e2
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-02-20 14:59:30 +0300

    Update ProfilePage.jsx

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -5,3 +5,12 @@ function ProfilePage({ userData }) {
 }
 
 export default ProfilePage;
+
+// src/ProfilePage.jsx
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
