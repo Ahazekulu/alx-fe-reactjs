eeaaf29f4fe96223b3033d70a47d36a32591ddd9
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-02-20 15:05:53 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -14,3 +14,23 @@ function ProfilePage() {
 }
 
 export default ProfilePage;
+
+
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react'; // Import React and useContext
+import UserContext from './UserContext'; // Import UserContext
+
+function UserProfile() {
+  // Consume the UserContext
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
