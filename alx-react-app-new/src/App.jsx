e17ef24f6ab2e65fbc037c84f562abb7afc3a450
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-02-20 14:51:07 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -48,3 +48,20 @@ function App() {
 }
 
 export default App;
+
+
+
+// src/App.jsx
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
