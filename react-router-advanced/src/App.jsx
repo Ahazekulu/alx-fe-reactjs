0927f2b2701e1b7972b08d82dbc1a667af2d288c
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-06 17:00:53 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-        <Route path="/profile"  element={<ProtectedRoute><Profile /></ProtectedRoute>}/>
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <div>
+        <h1>React Router Demo</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | 
+          <Link to="/post/1">Post 1</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}>
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
+          <Route path="/post/:id" element={<div>Dynamic Post ID: {window.location.pathname.split('/')[2]}</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
