fe800fe0ee7b03a91c9427446c92cf44149dc73d
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-06 16:32:47 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+<Route
+  path="/profile"
+  element={
+    <ProtectedRoute>
+      <Profile />
+    </ProtectedRoute>
+  }
+/>
 
 function App() {
   return (
