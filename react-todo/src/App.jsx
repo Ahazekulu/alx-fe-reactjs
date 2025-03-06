dff13b40697b29eb862585fc77db9cd8cd49f9cf
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-06 17:02:06 +0300

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import TodoList from "./components/TodoList";
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div>
-      <h1>Todo List</h1>
+      <h1>Todo List Demo</h1>
       <TodoList />
     </div>
   );
