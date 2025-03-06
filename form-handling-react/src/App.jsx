f86bba933522c2a20a1bb5c8ca315dec3bd09750
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-06 16:52:44 +0300

    Create App.jsx

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import formikForm from "./components/formikForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Registration Form (Controlled Components)</h1>
+      <RegistrationForm />
+
+      <h1>Registration Form (Formik)</h1>
+      <formikForm />
+    </div>
+  );
+}
+
+export default App;
