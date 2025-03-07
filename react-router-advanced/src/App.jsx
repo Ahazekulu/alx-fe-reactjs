64ace93a7c44460e6490403702b7fb1037b36b5d
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-07 11:15:56 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,29 +1,39 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
 import ProtectedRoute from './components/ProtectedRoute';
+import { useParams } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <h1>React Router Demo</h1>
+        <h1>React Router Advanced Demo</h1>
         <nav>
           <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | 
-          <Link to="/post/1">Post 1</Link>
+          <Link to="/post/1">Post 1</Link> | <Link to="/post/2">Post 2</Link>
         </nav>
         <Routes>
           <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}>
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-          <Route path="/post/:id" element={<div>Dynamic Post ID: {window.location.pathname.split('/')[2]}</div>} />
+          <Route
+            path="/profile/*" // Use wildcard to allow nested routes in Profile
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="*" element={<div>404 - Not Found</div>} />
         </Routes>
       </div>
     </BrowserRouter>
   );
+}
+
+// Dynamic route component
+function Post() {
+  const { id } = useParams();
+  return <div>Dynamic Post ID: {id}</div>;
 }
 
 export default App;
