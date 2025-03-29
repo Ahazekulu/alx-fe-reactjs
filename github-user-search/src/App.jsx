81916fdb5e7e837a1a3825f922a88f8937f8c506
Dagne Dereje <148545775+Ahazekulu@users.noreply.github.com>
2025-03-29 11:26:13 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,12 @@
-import Search from './components/Search';
+import { useState } from 'react'
 
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <Search />
+      <p>Welcome to the GitHub User Search App!</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
